86a61dc89e00864c83f78fb709bc015ced41a0e8
cristhian <ortizcristhian504@gmail.com>
2026-03-11 16:52:50 -0700

    Implementa clases Usuario Admin Alumno

---
+++ parcial-1-poo/examen-practico/Clases/Admin.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Clases;
+
+class Admin extends Usuario {
+
+    public function getRol(){
+        return "Administrador";
+    }
+
+}
---
+++ parcial-1-poo/examen-practico/Clases/Usuario.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Clases;
+
+use Exception;
+
+class Usuario {
+
+    protected $nombre;
+    protected $correo;
+
+    public function __construct($nombre, $correo){
+
+        if(!filter_var($correo, FILTER_VALIDATE_EMAIL)){
+            throw new Exception("Correo inválido: $correo");
+        }
+
+        $this->nombre = $nombre;
+        $this->correo = $correo;
+
+    }
+
+    public function getNombre(){
+        return $this->nombre;
+    }
+
+    public function getCorreo(){
+        return $this->correo;
+    }
+
+}
